22ff08eecce9ca4adbfe948c9c0c359d3e81f7aa
Subrajeet <subrajeetbiswas.0898@gmail.com>
2024-01-15 22:28:16 +0530

    Icon, Icon color, Content changed!

--- Prototype/ContentView.swift
+++ Prototype/ContentView.swift
@@ -10,10 +10,11 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "heart.fill")
+                .foregroundColor(.red)
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Hello, love!")
         }
         .padding()
     }
