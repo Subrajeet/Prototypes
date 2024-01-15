4858199623c78c13ecc0a86d2f567b9fd9be665f
Subrajeet <subrajeetbiswas.0898@gmail.com>
2024-01-15 21:58:05 +0530

    Initial Commit

---
+++ Prototype/PrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrototypeApp.swift
+//  Prototype
+//
+//  Created by Subrajeet Biswas on 15/01/24.
+//
+
+import SwiftUI
+
+@main
+struct PrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
